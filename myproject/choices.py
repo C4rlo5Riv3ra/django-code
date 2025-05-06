64afeca673fc60cf576d3c8ae9c2dc2aa5e1ecfe
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-05-06 01:55:55 -0500

    Update choices

--- myproject/choices.py
+++ myproject/choices.py
@@ -3,3 +3,9 @@ from django.db import models
 class estadoEntidades(models.IntegerChoices):
     ACTIVO = 1, 'Activo'
     DE_BAJA = 2, 'De baja'
+
+class EstadoOrden(models.IntegerChoices): 
+    PENDIENTE = 1, "Pendiente" 
+    PROCESANDO = 2, "Procesando" 
+    COMPLETADA = 3, "Completada" 
+    CANCELADA = 4, "Cancelada" 
